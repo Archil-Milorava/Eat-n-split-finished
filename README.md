f3821844be966de23da0edc434618674a15597f0
Archil-Milorava <achimilorava16@gmail.com>
2024-07-25 22:13:44 +0400

    starting project, tailwind and typescript installed

---
+++ README.md
@@ -0,0 +1,5 @@
+# Eat-n-split
+
+This is react project with typescript and tailwind CSS
+
+In this app you can calculate the bill with your friends
---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+type Friend = {
+  id: number;
+  name: string;
+  image: string;
+  balance: number;
+};
+
+const initialFriends: Friend[] = [
+  {
+    id: 121212,
+    name: 'Clark',
+    image: "https://i.pravatar.cc/48",
+    balance: -7
+  },
+  {
+    id: 121212,
+    name: 'Clark',
+    image: "https://i.pravatar.cc/48",
+    balance: -7
+  },
+  {
+    id: 121212,
+    name: 'Clark',
+    image: "https://i.pravatar.cc/48",
+    balance: -7
+  }
+]
+
+
+
+function App() {
+  return (
+    <div className="">App</div>
+  )
+}
+
+export default App
